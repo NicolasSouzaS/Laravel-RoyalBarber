e3f3dfdd914994c45969d1c58ac0d78340c1baf1
Gustavo Sampaio Soier <gsampaiosoier0@gmail.com>
2024-03-19 15:15:23 -0300

    Subindo arquivos da parte de login

--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -3,26 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <link rel="stylesheet" href="css/login.css">
+    <title>Tela de Login</title>
 </head>
 <body>
-
-    <div>
-        <div>
-            <form action="">
-                <label for="">Email:</label>
-                <input type="text">
-
-                <label for="">Senha</label>
-                <input type="text">
+    <div class="container">
+        <div class="image-container">
+            <img src="images/img_4.jpg" alt="Imagem de fundo">
+        </div>
+        <div class="login-form">
+            <h2>Faça Login</h2>
+            <form action="#">
+                <div class="input-container">
+                    <i class="fas fa-user"></i>
+                    <input type="text" placeholder="Usuário:" required>
+                </div>
+                <div class="input-container">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" placeholder="Senha:" required>
+                </div>
+                <button type="submit">Entrar</button>
             </form>
         </div>
-
-        <div>
-
-        </div>
     </div>
-
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </body>
 </html>
