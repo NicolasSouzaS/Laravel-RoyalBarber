b64d05a3f5b00bda6e826b8c1200dbb14198cbe7
Gustavo Sampaio Soier <gsampaiosoier0@gmail.com>
2024-03-19 17:24:44 -0300

    Criando a pááagina de lçogin e mudando a pagina de contato

--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -3,29 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset ('fonts/ionicons/css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset ('fonts/fontawesome/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset ('fonts/flaticon/font/flaticon.css') }}">
+
     <link rel="stylesheet" href="css/login.css">
-    <title>Tela de Login</title>
+    <title>Login - RoyalBarber</title>
 </head>
 <body>
     <div class="container">
-        <div class="image-container">
-            <img src="images/img_4.jpg" alt="Imagem de fundo">
-        </div>
-        <div class="login-form">
-            <h2>Faça Login</h2>
-            <form action="#">
-                <div class="input-container">
-                    <i class="fas fa-user"></i>
-                    <input type="text" placeholder="Usuário:" required>
+        <div class="login-box">
+            <img src="{{ asset ('images/bg-login.jpeg') }}" alt="">
+            <h2>Bem-vindo!!!</h2>
+            <form action="{{ route ('login') }}" method="POST" class="login-form">
+                @csrf
+                <div class="textbox">
+                    <input type="email" name="email" placeholder="Email" value="{{ old('email') }}">
+                    {{ $errors->has('email') ? $errors->first('email') : '' }}
                 </div>
-                <div class="input-container">
-                    <i class="fas fa-lock"></i>
-                    <input type="password" placeholder="Senha:" required>
+                <div class="textbox">
+                    <input type="password" name="password" placeholder="Senha" value="{{ old ('password') }}">
+                    {{ $errors->has('password') ? $errors->first('password') : '' }}
                 </div>
-                <button type="submit">Entrar</button>
+                <input type="submit" class="btn" value="Entrar">
             </form>
         </div>
     </div>
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </body>
 </html>
